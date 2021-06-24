ab72351d6d77ff1822d52724c3471e930b87ab81
byronalegre <alegre_byron@hotmail.com>
2021-06-24 11:48:42 -0300

    Agregar archivos de proyecto.

---
+++ Migrations/20210623145353_v1.0.0.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BEComentarios.Migrations
+{
+    public partial class v100 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Comentario",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    titulo = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    creador = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    texto = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    fechaComentario = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comentario", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comentario");
+        }
+    }
+}
---
+++ Models/Comentario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BEComentarios.Models
+{
+    public class Comentario
+    {
+        public int id { get; set; }
+        [Required]
+        public string titulo { get; set; }
+        [Required]
+        public string creador { get; set; }
+        [Required]
+        public string texto { get; set; }
+        [Required]
+        public DateTime fechaComentario { get; set; }
+
+        internal static Task FirstOrDefault(Func<object, bool> p)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static Task FirstOrDefault(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
